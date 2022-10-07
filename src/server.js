0102daccd1e89e08262eb87ba12f95d5c99edc4d
rohangoli <rohanr27@gmail.com>
2022-10-07 14:21:53 -0400

    Dynamic Webapge : Fix date diff

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Add html-entities to avoid XSS cross scripting
 const {encode} = require('html-entities');
 
+// Add Cookie-parser
+const cookieParser = require('cookie-parser');
+
 // create a new server application
 const app = express();
 
@@ -19,26 +22,62 @@ app.use(express.static('public'))
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// Instantiate App
+app.use(cookieParser());
+
+// Add visitorId
+let nextVisitorId = 1;
+
 // The main page of our website
 app.get('/', (req, res) =>{
+    // console.log(req.headers.cookie);
+    console.log(req.cookies);
 
-    // reads the url parameter - http://domain/?name=text
-    const name = req.query.name || "World";
+    // Set/Get Visitor Id Cookie
+    let visitorId;
+    if(req.cookies.visitorId == undefined){
+        visitorId = nextVisitorId++;
+        res.cookie('visitorId', visitorId);
+    }
+    else{
+        visitorId = req.cookies.visitorId;
+    }
 
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8" />
-                <title>An Example Title</title>
-                <link rel="stylesheet" href="app.css">
-            </head>
-            <body>
-                <h1>Hello, ${encode(name)}!</h1>
-                <p>HTML is so much better than a plain string!</p>
-            </body>
-        </html>
-    `);
+    // Set Visited TimeStamp and Get Last Visit Time
+    let lastVisited;
+    if(req.cookies.visited){
+        timediff = Math.round((Date.now().toString()-req.cookies.visited)/(1000));
+        lastVisited = "It has been "+timediff+" seconds since your last visit.";
+    }
+    else{
+        lastVisited = "you have never visited";
+    }
+    res.cookie('visited', Date.now().toString());   
+    
+    // Render EJS Template with Variables
+    res.render('welcome', {
+        name: encode(req.query.name) || "World",
+        datestring: new Date().toLocaleString(),
+        visitorId: visitorId,
+        lastVisited: lastVisited,
+    });
+
+    // // reads the url parameter - http://domain/?name=text
+    // const name = req.query.name || "World";
+    // res.send(`
+    //     <!DOCTYPE html>
+    //     <html lang="en">
+    //         <head>
+    //             <meta charset="UTF-8" />
+    //             <title>An Example Title</title>
+    //             <link rel="stylesheet" href="app.css">
+    //         </head>
+    //         <body>
+    //             <h1>Hello, ${encode(name)}!</h1>
+    //             <p>HTML is so much better than a plain string!</p>
+    //         </body>
+    //     </html>
+    // `);
 });
 
 // Start listening for network connections
