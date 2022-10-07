dbad82c28a2b5203c19495ecfeb116685a6c5184
rohangoli <rohanr27@gmail.com>
2022-10-07 09:59:32 -0400

    Update before ejs template

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 // use the express library
 const express = require('express');
 
+// Add html-entities to avoid XSS cross scripting
+const {encode} = require('html-entities');
+
 // create a new server application
 const app = express();
 
@@ -10,9 +13,32 @@ const app = express();
 // world wibe web
 const port = process.env.PORT || 3000;
 
+// Serve Static Files
+app.use(express.static('public'))
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // The main page of our website
 app.get('/', (req, res) =>{
-    res.send('Hello World!')
+
+    // reads the url parameter - http://domain/?name=text
+    const name = req.query.name || "World";
+
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <title>An Example Title</title>
+                <link rel="stylesheet" href="app.css">
+            </head>
+            <body>
+                <h1>Hello, ${encode(name)}!</h1>
+                <p>HTML is so much better than a plain string!</p>
+            </body>
+        </html>
+    `);
 });
 
 // Start listening for network connections
